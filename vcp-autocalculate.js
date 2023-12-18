e148f12853172fcd1f9035a943c91b9f7db636b0
frnprt <44530535+frnprt@users.noreply.github.com>
2023-12-18 18:33:49 +0100

    Working version with selector

--- vcp-autocalculate.js
+++ vcp-autocalculate.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/frnprt/vcp-autocalculator
 // @description Automatically computes monthly gains from VCP site
 // @match       http://www.principatumpapiae.com/scheda_euro.php
-// @version     0.1
+// @version     0.5
 // @updateURL   
 // @dowloadURL  
 // @author      frnprt
@@ -16,24 +16,60 @@
 (function() {
     'use strict';
 
-    // Create a JSON object from the table of the financial movements of the last month
-    var table = $('#movimenti_1').tableToJSON({
-        headings: ['', 'data_operazione', 'entrate', 'uscite', 'erogante', 'beneficiario'],
-        ignoreHiddenRows: false
-    });
-    // Delete even entries to cleanup spurious rows whose fields are filled with descriptors (e.g. "Giustizia-Trasferimento")
-    var elements_to_delete = [...Array(table.length).keys()].filter(element => element % 2 == 0);
-    _.pullAt(table, elements_to_delete)
+    // Compute the net money for a given month;
+    // @param month_number: the position of the month in the list of currently displayed months (i.e. 1,2,3 etc.), starting from 0;
+    // example: if December 2023 and November 2023 are displayed in this order in "scheda_euro.php", December 2023 would be 0 and November 2023 would be 1.
+    function compute_net_for_month(month_number){
+        // Create a JSON object from the table of the financial movements of the month
+        // Month number is increased by 1 to take in account the off-by-one enumeration of money movements tables in the HTML code
+        var table = $(`#movimenti_${parseInt(month_number) + 1}`).tableToJSON({
+            headings: ['', 'data_operazione', 'entrate', 'uscite', 'erogante', 'beneficiario'],
+            ignoreHiddenRows: false
+        });
+        // Delete even entries to cleanup spurious rows whose fields are filled with action descriptors (e.g. "Giustizia-Trasferimento")
+        var elements_to_delete = [...Array(table.length).keys()].filter(element => element % 2 == 0);
+        _.pullAt(table, elements_to_delete)
 
-    // Compute the total sum of the movements
-    var sum = 0;
-    table.forEach(element => {
-        if (element.entrate) {
-            sum += parseFloat(element.entrate);
-        } else {
-            sum += parseFloat(element.uscite);
-        }
+        // Compute the total sum of the movements
+        var sum = 0;
+        table.forEach(element => {
+            if (element.entrate) {
+                sum += parseFloat(element.entrate);
+            } else {
+                sum += parseFloat(element.uscite);
+            }
+        });
+        return sum.toFixed(2);
+    }
+
+    // RENDER UI ELEMENTS:
+    // Get the months' headers elements
+    const months = document.querySelectorAll("tr[id^=header_mesi]");
+    console.log(months);
+    // Build a map that associates every month displayed with the same digit key used in the other DOM tables
+    const months_map = new Map();
+    months.forEach(element => {
+        // Let's not make this dirty with 5 lines of JS regexp generator;
+        // KISS; change at your leisure.
+        months_map.set(element.id.trim().replace(/^\D+/g, ''), element.innerText.trim());
     });
-    console.log(table);
-    console.log(sum.toFixed(2));
+    console.log(months);
+    console.log(months_map);
+    // Create new DOM elements
+    const month_selector = document.createElement("month_selector");
+    var month_selector_html = `<select name="months" id="months" class="textfield-scuro">`;
+    for (const [key, value] of months_map) {
+        month_selector_html += `<option value="${key}">${value}</option>`;
+    };
+    month_selector_html += "</select>";
+    month_selector.innerHTML = month_selector_html;
+
+    const sum_display = document.createElement("sum_display");
+    sum_display.innerHTML = "<div class=\"big\">Seleziona un mese di cui vuoi vedere il netto:<div>";
+    document.body.appendChild(sum_display);
+    document.body.appendChild(month_selector);
+
+    month_selector.addEventListener("change", (event) => {
+        sum_display.firstChild.textContent = compute_net_for_month(event.target.value);
+    });
 })();
