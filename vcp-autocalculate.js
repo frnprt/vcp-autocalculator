174edb8ccdfcdfdbae4b6fcdd70dbc983871c251
frnprt <44530535+frnprt@users.noreply.github.com>
2023-12-18 12:57:41 +0100

    Add script header

---
+++ vcp-autocalculate.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        vcp-autocalculate
+// @namespace   https://github.com/frnprt/vcp-autocalculator
+// @description Automatically computes monthly gains from VCP site
+// @match       http://www.principatumpapiae.com/scheda_euro.php
+// @version     0.1
+// @updateURL   
+// @dowloadURL  
+// @author      frnprt
+// @grant       none
+// ==/UserScript==
+
